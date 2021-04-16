158677368088ba2fb1d44ecb5fada42dc14d2040
alextagun <alextagun@gmail.com>
2021-04-16 15:10:52 +0300

    add MotorcycleController

---
+++ Assets/Scripts/MotorcycleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorcycleController : MonoBehaviour {
+    [SerializeField] private Rigidbody2D backWheel; 
+    [SerializeField] private Rigidbody2D frontWheel;
+    [SerializeField]private float speed;
+    
+    private float _movement;
+
+    private void Update() {
+        _movement = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate() {
+        backWheel.AddTorque(_movement * speed * Time.fixedDeltaTime);
+        frontWheel.AddTorque(_movement * speed * Time.fixedDeltaTime);
+    }
+}
