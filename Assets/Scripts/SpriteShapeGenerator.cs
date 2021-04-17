397cbde6afc1048bb93c102d645f9cc3ef711c18
alextagun <alextagun@gmail.com>
2021-04-17 13:57:27 +0300

    add params for terrain generator

--- Assets/Scripts/SpriteShapeGenerator.cs
+++ Assets/Scripts/SpriteShapeGenerator.cs
@@ -8,13 +8,18 @@ public class SpriteShapeGenerator : MonoBehaviour {
     [SerializeField] private SpriteShapeController spriteShape;
     [SerializeField] private EdgeCollider2D _collider;
 
+    private float _cellWidth;
+    public void SetData(float cellWidth) {
+        _cellWidth = cellWidth;
+    }
+
     public void GenerateShape(float[] heights) {
         var spline = spriteShape.spline;
         spline.Clear();
 
         int i = 0;
         for (i = 0; i < heights.Length; i++) {
-            spline.InsertPointAt(i, new Vector3(i * 2, heights[i], 0));
+            spline.InsertPointAt(i, new Vector3(i * _cellWidth, heights[i], 0));
             
             if (i == heights.Length - 1 || i == 0) {
                 spline.SetTangentMode(i, ShapeTangentMode.Linear);
@@ -26,13 +31,11 @@ public class SpriteShapeGenerator : MonoBehaviour {
             spline.SetRightTangent(i, new Vector3(1, 0, 0));
         }
         
-        spline.InsertPointAt(i, new Vector3((i - 1) * 2, -5, 0));
+        spline.InsertPointAt(i, new Vector3((i - 1) * _cellWidth, -5, 0));
         spline.SetTangentMode(i, ShapeTangentMode.Linear);
         spline.InsertPointAt(i + 1, new Vector3(0, -5, 0));
         spline.SetTangentMode(i + 1, ShapeTangentMode.Linear);
         
-        // spriteShape.RefreshSpriteShape();
-        // spriteShape.UpdateSpriteShapeParameters();
         spriteShape.BakeMesh();
         spriteShape.BakeCollider();
         
@@ -43,16 +46,5 @@ public class SpriteShapeGenerator : MonoBehaviour {
         // }
         //
         // _collider.points = list.ToArray();
-    }
-
-    private void LateUpdate() {
-        // spriteShape.BakeMesh();
-        // spriteShape.BakeCollider();
-    }
-
-    private float[] GenerateHeights() {
-        return new float[] {
-            0, 0, 0, 0, 1, 2, 3, 2, 1, 1, 1, 0, 3, 4, 3, 2, 1, 0, 1, 0,
-        };
     }
 }
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -4,15 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour {
+    [SerializeField] private int heightsCount = 20;
+    [SerializeField] private int cellWidth = 2;
+    
     [SerializeField] private SpriteShapeGenerator A;
     [SerializeField] private SpriteShapeGenerator B;
 
     private readonly HeightsGenerator _heightsGenerator = new HeightsGenerator();
     
-    private int _heightsCount = 20;
+    
 
     private void Start() {
-        A.GenerateShape(_heightsGenerator.Generate(_heightsCount));
+        A.SetData(cellWidth);
+        B.SetData(cellWidth);
+        
+        A.GenerateShape(_heightsGenerator.Generate(heightsCount));
         GenerateNextSpriteShape();
     }
 
@@ -20,12 +26,14 @@ public class TerrainGenerator : MonoBehaviour {
         bool isALast = A.transform.position.x > B.transform.position.x;
         SpriteShapeGenerator lastShape = isALast ? A : B;
         SpriteShapeGenerator firstShape = !isALast ? A : B;
-
-        Vector3 newPosition = new Vector3(lastShape.transform.position.x + 38, 0, 0);
-        firstShape.transform.position = newPosition;
-        transform.position = newPosition + new Vector3(19, 0, 0);
         
-        firstShape.GenerateShape(_heightsGenerator.Generate(_heightsCount));
+        float shapeWidth = cellWidth * (heightsCount - 1);
+
+        Vector3 newPosition = new Vector3(lastShape.transform.position.x + shapeWidth, 0, 0);
+        firstShape.transform.position = newPosition;
+        transform.position = newPosition + new Vector3(shapeWidth / 2, 0, 0);
+        
+        firstShape.GenerateShape(_heightsGenerator.Generate(heightsCount));
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
