e9ae2ed16990bb4dd8c9ca3247e103b1c3744251
alextagun <alextagun@gmail.com>
2021-04-17 18:38:06 +0300

    add distance logic

---
+++ Assets/Scripts/UiDistance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UiDistance : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private Transform motorcycle;
+    
+    private float _startPosition;
+
+    private void Start() {
+        _startPosition = motorcycle.position.x;
+    }
+
+    private void Update() {
+        int dif = Mathf.RoundToInt(motorcycle.position.x - _startPosition);
+        if (dif < 0) dif = 0;
+        _text.text = dif.ToString();
+    }
+}
