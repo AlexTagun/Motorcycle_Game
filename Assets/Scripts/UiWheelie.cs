2c86a20479bede82f01373098b72cc1e6d63076a
alextagun <alextagun@gmail.com>
2021-04-17 19:01:32 +0300

    add wheelie logic

---
+++ Assets/Scripts/UiWheelie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiWheelie : MonoBehaviour {
+    [SerializeField] private CircleCollider2D backWheel;
+    [SerializeField] private CircleCollider2D frontWheel;
+    [SerializeField] private Collider2D terrainA;
+    [SerializeField] private Collider2D terrainB;
+    [SerializeField] private GameObject text;
+
+    private float _fadeTime = 0.2f;
+    private float _timeLeft = 0;
+
+    private void Update() {
+        Debug.Log($"backWheel = {backWheel.IsTouching(terrainA)}, frontWheel = {frontWheel.IsTouching(terrainA)}");
+
+        if (backWheel.IsTouching(terrainA) && !frontWheel.IsTouching(terrainA)
+            || backWheel.IsTouching(terrainB) && !frontWheel.IsTouching(terrainB)) {
+            _timeLeft = _fadeTime;
+        }
+        
+        text.SetActive(false);
+        if(_timeLeft < 0) return;
+
+        _timeLeft -= Time.deltaTime;
+
+        text.SetActive(true);
+
+    }
+}
