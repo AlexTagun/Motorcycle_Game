f3b587ae86382ab38dd6106238af96516ad8dfe2
alextagun <alextagun@gmail.com>
2021-04-17 14:38:20 +0300

    fix MotorcycleController

--- Assets/Scripts/MotorcycleController.cs
+++ Assets/Scripts/MotorcycleController.cs
@@ -10,21 +10,39 @@ public class MotorcycleController : MonoBehaviour {
 
     private JointMotor2D _motor;
     private float _movement;
+    private bool _anyKeyPressed;
 
     private void Update() {
+        _anyKeyPressed = false;
 #if UNITY_EDITOR
-        _movement = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.D)) {
+            _movement = 1;
+            _anyKeyPressed = true;
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            _movement = 0;
+            _anyKeyPressed = true;
+            return;
+        }
+        
         return;
 #endif
         if(Input.touchCount < 1) return;
 
         bool isRightSideClick = Input.touches[0].position.x > Screen.width / (float) 2;
-
+        _anyKeyPressed = true;
         _movement = isRightSideClick ? 1 : 0;
 
     }
 
     private void FixedUpdate() {
+        backWheel.useMotor = _anyKeyPressed;
+        frontWheel.useMotor = _anyKeyPressed;
+        
+        if(!_anyKeyPressed) return;
+        
         _motor.maxMotorTorque = 999999999;
         _motor.motorSpeed = _movement * speed;
         backWheel.motor = _motor;
